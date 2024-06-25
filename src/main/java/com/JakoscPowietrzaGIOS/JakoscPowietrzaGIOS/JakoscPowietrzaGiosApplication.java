d6f634333be67a0eb2201f0b0167e5db709b0e5b
Paweł Michalski <pmpawel126@gmail.com>
2024-06-25 15:02:26 +0200

    Commiting all files to the repository

---
+++ src/main/java/com/JakoscPowietrzaGIOS/JakoscPowietrzaGIOS/JakoscPowietrzaGiosApplication.java
@@ -0,0 +1,13 @@
+package com.JakoscPowietrzaGIOS.JakoscPowietrzaGIOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakoscPowietrzaGiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakoscPowietrzaGiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JakoscPowietrzaGIOS/JakoscPowietrzaGIOS/JakoscPowietrzaGiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JakoscPowietrzaGIOS.JakoscPowietrzaGIOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakoscPowietrzaGiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
